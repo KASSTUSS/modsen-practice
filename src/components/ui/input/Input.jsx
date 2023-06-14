ef8a2abebc74de5b18e1020673eaa3f4c9fce939
KASSTUSS <kostya2003112015@gmail.com>
2023-06-14 23:17:59 +0300

    fix: fix display of search icon

--- src/components/ui/input/Input.jsx
+++ src/components/ui/input/Input.jsx
@@ -32,7 +32,7 @@ const Input = (props) => {
                     className={styles.searchButton}
                     onClick={onClickButton}
                 >
-                    <img className={styles.searchButton__icon} src="../../../../public/img/search.svg" alt="" />
+                    <svg className={styles.searchButton__icon} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14" height="96" width="96"><path stroke="#000000" strokeLinecap="round" strokeLinejoin="round" d="M5.92 11.34C8.91338 11.34 11.34 8.91338 11.34 5.92C11.34 2.92662 8.91338 0.5 5.92 0.5C2.92662 0.5 0.5 2.92662 0.5 5.92C0.5 8.91338 2.92662 11.34 5.92 11.34Z"></path><path stroke="#000000" strokeLinecap="round" strokeLinejoin="round" d="M13.5 13.5L9.75 9.75"></path></svg>
                 </button>
             )}
         </div>
